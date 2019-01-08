836d7de8a48040f26f8cfd5ad019184863e574da
Rahul Kumar <rahul.kumarrkr96@gmail.com>
2019-01-08 10:30:39 +0530

    Add files via upload

--- ytserver.py
+++ ytserver.py
@@ -12,6 +12,163 @@ class SQLoperations:
         b = data[i + 1: ]
         return a + b
 
+    def fetchFilteredData(self, tablename,fieldname,value):
+        global conn
+        query = "SELECT * from " + tablename + " WHERE " + fieldname "="+value
+        response = ""
+        flag = False
+
+        if (tablename == "booking"):
+            cursor = conn.execute(query)
+            response += "{\"booking\":\"["
+            for row in cursor:
+                flag = True
+                response +=  "{\"id\":"+str(row[0])+","
+                response +=  "\"tour_name\":\""+str(row[1])+"\","
+                response +=  "\"city_id\":"+str(row[2])+","
+                response +=  "\"date\":\""+str(row[3])+"\","
+                response +=  "\"start_time\":\""+str(row[4])+"\","
+                response +=  "\"number_of_people\":"+str(row[5])+","
+                response +=  "\"discount\":"+str(row[6])+","
+                response +=  "\"payment_type_id\":"+str(row[7])+","
+                response +=  "\"channel_id\":"+str(row[8])+","
+                response +=  "\"guest_id\":"+str(row[9])+","
+                response +=  "\"booking_reference\":\""+str(row[10])+"\","
+                response +=  "\"storyteller_id\":"+str(row[11])+"},"
+            response += "]\"}"
+            if(flag == True):
+                response = self.removeCharFromString(response, -4)
+            print(response+"\n")
+
+        elif (tablename == "booking_channel"):
+            cursor = conn.execute(query)
+            response += "{\"booking_channel\":\"["
+            for row in cursor:
+                flag = True
+                response +=  "{\"id\":"+str(row[0])+","
+                response +=  "\"name\":\""+str(row[1])+"\","
+                response +=  "\"details\":\""+str(row[2])+"\"},"
+            response += "]\"}"
+            if(flag == True):
+                response = self.removeCharFromString(response, -4)
+            print(response+"\n")
+
+        elif (tablename == "city"):
+            cursor = conn.execute(query)
+            response += "{\"city\":\"["
+            for row in cursor:
+                flag = True
+                response +=  "{\"id\":"+str(row[0])+","
+                response +=  "\"name\":\""+str(row[1])+"\"},"
+            response += "]\"}"
+            if(flag == True):
+                response = self.removeCharFromString(response, -4)
+            print(response+"\n")
+
+        elif (tablename == "guest"):
+            cursor = conn.execute(query)
+            response += "{\"guest\":\"["
+            for row in cursor:
+                flag = True
+                response +=  "{\"id\":"+str(row[0])+","
+                response +=  "\"name\":\""+str(row[1])+"\","
+                response +=  "\"mobile_number\":\""+str(row[2])+"\","
+                response +=  "\"email\":\""+str(row[3])+"\","
+                response +=  "\"details\":\""+str(row[4])+"\"},"
+            response += "]\"}"
+            if(flag == True):
+                response = self.removeCharFromString(response, -4)
+            print(response+"\n")
+
+        elif (tablename == "ops_executive"):
+            cursor = conn.execute(query)
+            response += "{\"ops_executive\":\"["
+            for row in cursor:
+                flag = True
+                response +=  "{\"id\":"+str(row[0])+","
+                response +=  "\"password\":\""+str(row[1])+"\","
+                response +=  "\"name\":\""+str(row[2])+"\","
+                response +=  "\"designation\":\""+str(row[3])+"\","
+                response +=  "\"city_id\":"+str(row[4])+","
+                response +=  "\"email\":\""+str(row[5])+"\"},"
+            response += "]\"}"
+            if(flag == True):
+                response = self.removeCharFromString(response, -4)
+            print(response+"\n")
+
+        elif (tablename == "payment_type"):
+            cursor = conn.execute(query)
+            response += "{\"payment_type\":\"["
+            for row in cursor:
+                flag = True
+                response +=  "{\"id\":"+str(row[0])+","
+                response +=  "\"name\":\""+str(row[1])+"\"},"
+            response += "]\"}"
+            if(flag == True):
+                response = self.removeCharFromString(response, -4)
+            print(response+"\n")
+
+        elif (tablename == "story_teller"):
+            cursor = conn.execute(query)
+            response += "{\"story_teller\":\"["
+            for row in cursor:
+                flag = True
+                response +=  "{\"id\":"+str(row[0])+","
+                response +=  "\"name\":\""+str(row[1])+"\","
+                response +=  "\"city_id\":"+str(row[2])+","
+                response +=  "\"exp_level\":"+str(row[3])+","
+                response +=  "\"salary_per_month\":"+str(row[4])+","
+                response +=  "\"cash_holding\":"+str(row[5])+","
+                response +=  "\"number_of_tours\":"+str(row[6])+","
+                response +=  "\"per_tour_fee\":"+str(row[7])+","
+                response +=  "\"per_paid_tour_fee\":"+str(row[8])+"},"
+            response += "]\"}"
+            if(flag == True):
+                response = self.removeCharFromString(response, -4)
+            print(response+"\n")
+		
+		elif (tablename == "storyteller_avaliability"):
+            cursor = conn.execute(query)
+            response += "{\"storyteller_avaliability\":\"["
+            for row in cursor:
+                flag = True
+                response +=  "{\"date\":"+str(row[0])+","
+                response +=  "\"story_teller_ids\":\""+str(row[1])+"\","
+            response += "]\"}"
+            if(flag == True):
+                response = self.removeCharFromString(response, -4)
+            print(response+"\n")
+
+        elif (tablename == "tour"):
+            cursor = conn.execute(query)
+            response += "{\"tour\":\"["
+            for row in cursor:
+                flag = True
+                response +=  "{\"id\":"+str(row[0])+","
+                response +=  "\"name\":\""+str(row[1])+"\","
+                response +=  "\"type_id\":"+str(row[2])+","
+                response +=  "\"city_id\":"+str(row[3])+","
+                response +=  "\"start_time\":\""+str(row[4])+"\","
+                response +=  "\"duration\":"+str(row[5])+","
+                response +=  "\"price\":"+str(row[6])+"},"
+            response += "]\"}"
+            if(flag == True):
+                response = self.removeCharFromString(response, -4)
+            print(response+"\n")
+
+        elif (tablename == "tour_type"):
+            cursor = conn.execute(query)
+            response += "{\"tour_type\":\"["
+            for row in cursor:
+                flag = True
+                response +=  "{\"id\":"+str(row[0])+","
+                response +=  "\"name\":\""+str(row[1])+"\"},"
+            response += "]\"}"
+            if(flag == True):
+                response = self.removeCharFromString(response, -4)
+            print(response+"\n")
+
+        return response
     def fetchData(self, tablename):
         global conn
         query = "SELECT * from " + tablename
@@ -215,8 +372,12 @@ class S(BaseHTTPRequestHandler):
         if (post_data.find('SELECT') != -1): 
             print ("Performing SELECT operation")
             post_data_list = post_data.split()
-            # Check for WHERE(For including WHERE API...) To-Do sqlobj.fetchFilteredData()
-            status = sqlobj.fetchData(post_data_list[-1])
+            
+            # Check for WHERE(For including WHERE API...) 
+            if(post_data.find('WHERE') != -1):
+            	status = sqlobj.fetchFilteredData(post_data_list[3],post_data_list[5],post_data_list[7]);
+            else:	
+            	status = sqlobj.fetchData(post_data_list[-1])
             self.wfile.write(status)
         elif (post_data.find('INSERT') != -1): 
             print ("Performing INSERT operation")
